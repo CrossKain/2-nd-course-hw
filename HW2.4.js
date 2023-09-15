1d2fcf5cdc9bdb30282b8232ccc606b5b10c22cf
Denis <densweetfit@gmail.com>
2023-09-15 12:46:53 +0500

    Прошу Проверить ДЗ 2.4

--- HW2.4.js
+++ HW2.4.js
@@ -2,13 +2,13 @@
 // Задание 1
 
 for (let word = 0; word <= 1; word++) {
-    alert("Привет")
+    console.log("Привет")
 }
 
 //Задание 2
 const n = 5;
 for (let figure = 1; figure <= n; figure++) {
-    alert(figure);
+    console.log(figure);
 
 }
 
@@ -16,7 +16,7 @@ for (let figure = 1; figure <= n; figure++) {
 
 const a = 22;
 for (let score = 7; score <= a; score++) {
-    alert(score);
+    console.log(score);
 
 }
 
@@ -31,7 +31,7 @@ let object = {
 
 
 for (x in object) {
-    alert(x + " - зарплата " + object[x] + ' долларов');
+    console.log(x + " - зарплата " + object[x] + ' долларов');
 }
 
 //Задание 5
@@ -39,43 +39,62 @@ for (x in object) {
 for (let l = 1000, num = 0; l > 50; num++) {
     l /= 2;
     alert(l);
-    alert('Количество итераций =' + num);
+    console.log('Количество итераций =' + num);
+
 
 }
 
 // задание 6
 
-const week = {
-    'Понедельник': 1,
-    'Вторник': 2,
-    'Среда': 3,
-    'Четверг': 4,
-    'Пятница': 5,
-    'Суббота': 6,
-    'Воскресение': 7,
+// const week = {
+//     'Понедельник': 1,
+//     'Вторник': 2,
+//     'Среда': 3,
+//     'Четверг': 4,
+//     'Пятница': 5,
+//     'Суббота': 6,
+//     'Воскресение': 7,
 
 
 
-}
+// }
 
-for (let day in week) {
-    if (day == 'Пятница') {
-
-
-
-        alert('Сегодня пятница ' + week[day] + "число. Необходимо подготовить отчет");
-        weekscore += 1;
-        while (week[day] <= 31) {
-            week[day] += 7;
-            alert('Сегодня пятница ' + week[day] + "число. Необходимо подготовить отчет");
-        }
+// for (let day in week) {
+//     if (day == 'Пятница') {
 
 
 
+//         alert('Сегодня пятница ' + week[day] + "число. Необходимо подготовить отчет");
+//         weekscore += 1;
+//         while (week[day] <= 31) {
+//             week[day] += 7;
+//             alert('Сегодня пятница ' + week[day] + "число. Необходимо подготовить отчет");
+//         }
+
+
+
+//     }
+
+
+// }
+for (let fry = 4; fry <= 31; index++) {
+    console.log('Сегодня пятница ' + fry + "число. Необходимо подготовить отчет");
+    while (fry <= 31) {
+        fry += 7;
+        if (fry > 31) {
+            break;
+            
+        } 
+        console.log('Сегодня пятница ' + fry + "число. Необходимо подготовить отчет");
+           
     }
-
-
+    
+    
 }
+
+
+
+
 
 
 
